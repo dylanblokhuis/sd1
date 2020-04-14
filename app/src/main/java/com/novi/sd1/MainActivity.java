d1b4d1ebe650a6b27f0005b8dbcf0357ed9b0cb5
dylanblokhuis <dylanblokhuis3@hotmail.com>
2020-04-14 16:22:57 +0200

    buttons and buttons

--- app/src/main/java/com/novi/sd1/MainActivity.java
+++ app/src/main/java/com/novi/sd1/MainActivity.java
@@ -1,30 +1,78 @@
 package com.novi.sd1;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
-
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import com.novi.sd1.ui.main.SectionsPagerAdapter;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.novi.sd1.fragments.CameraDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
 
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraDialog();
+            }
+        });
     }
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    private void cameraDialog() {
+        CameraDialog dialog = new CameraDialog(this);
+        dialog.show();
+
+        Button libraryButton = dialog.getLibraryButton();
+        Button cameraButton = dialog.getCameraButton();
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
+        libraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        System.out.println("test");
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (RESULT_OK == resultCode) {
+//            // Get Extra from the intent
+//            Bundle extras = data.getExtras();
+//            // Get the returned image from extra
+//            Bitmap bmp = (Bitmap) extras.get("data");
+//
+////            ImageView iv = (ImageView) findViewById(R.id.ReturnedImageView);
+////            iv.setImageBitmap(bmp);
+//        }
+//    }
 }
---
+++ app/src/main/java/com/novi/sd1/fragments/CameraDialog.java
@@ -0,0 +1,44 @@
+package com.novi.sd1.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.novi.sd1.R;
+
+public class CameraDialog {
+    private Dialog builder;
+    private Button libraryButton;
+    private Button cameraButton;
+
+    public CameraDialog(AppCompatActivity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(R.string.camera_dialog_title)
+                .setView(R.layout.camera_dialog)
+                .setNegativeButton(R.string.camera_dialog_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+
+        this.builder = builder.create();
+    }
+
+    public void show() {
+        this.builder.show();
+
+        this.libraryButton = this.builder.findViewById(R.id.library_button);
+        this.cameraButton = this.builder.findViewById(R.id.camera_button);
+    }
+
+    public Button getLibraryButton() {
+        return libraryButton;
+    }
+
+    public Button getCameraButton() {
+        return cameraButton;
+    }
+}
